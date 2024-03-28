222fcbcb6006177c4a1b331edd98a564460f2e92
AlexeyPodkovko <alexey.podkovko@gmail.com>
2024-03-28 20:32:54 +0300

    - Added javadocs and the usage of HashMap has been replaced with ConcurrentHashMap to prevent issues in a multithreaded environment.

--- src/main/java/org/sportradar/MatchScoreboard.java
+++ src/main/java/org/sportradar/MatchScoreboard.java
@@ -6,12 +6,43 @@ import org.sportradar.validator.MatchValidator;
 import org.sportradar.validator.ValidationUtils;
 
 import java.time.Instant;
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * The {@code MatchScoreboard} class manages sports matches, including starting new matches,
+ * updating scores, finishing matches, and providing a summary of matches in progress. It uses
+ * a thread-safe approach with {@code ConcurrentHashMap} to ensure safe access and modification
+ * of match data across multiple threads.
+ *
+ * <p>Each match is identified by a unique ID and contains information about the home and away teams,
+ * as well as the current score and start time. This class provides methods to manipulate and query
+ * this data in a thread-safe manner.</p>
+ *
+ * <p>It relies on {@code MatchValidator} for validating country names and ensuring that match
+ * teams do not duplicate. Also, it uses {@code ValidationUtils} for null checks, adhering to
+ * a fail-fast approach to avoid illegal states.</p>
+ *
+ * @author Alexey Podkovko
+ */
 public class MatchScoreboard {
 
-    private final Map<String, Match> activeMatches = new HashMap<>();
+    private final Map<String, Match> activeMatches = new ConcurrentHashMap<>();
 
+    /**
+     * Starts a new match with the given team names and start time. Validates the countries and
+     * ensures they are not duplicates before adding the match to the active matches list.
+     *
+     * @param homeTeam The name of the home team.
+     * @param awayTeam The name of the away team.
+     * @param startTime The start time of the match.
+     * @return A unique match ID for the newly started match.
+     * @throws InvalidCountryNameException If the country names do not pass validation.
+     * @throws DuplicateCountryException If the home and away team names are the same.
+     */
     public String startNewMatch(final String homeTeam, final String awayTeam, final Instant startTime) throws InvalidCountryNameException, DuplicateCountryException {
         ValidationUtils.requireNonNull(homeTeam, awayTeam, startTime);
         MatchValidator.getInstance().validateCountries(homeTeam, awayTeam);
@@ -24,6 +55,14 @@ public class MatchScoreboard {
         return matchId;
     }
 
+    /**
+     * Updates the score for a match identified by the given match ID.
+     *
+     * @param matchId The unique identifier of the match.
+     * @param homeScore The new score for the home team.
+     * @param awayScore The new score for the away team.
+     * @throws IllegalArgumentException If any of the arguments are null.
+     */
     public void updateScore(final String matchId, final Integer homeScore,  final Integer awayScore) {
         ValidationUtils.requireNonNull(matchId, homeScore, awayScore);
 
@@ -35,12 +74,24 @@ public class MatchScoreboard {
         }
     }
 
+    /**
+     * Finishes a match by removing it from the list of active matches based on the given match ID.
+     *
+     * @param matchId The unique identifier of the match to finish.
+     * @throws IllegalArgumentException If the matchId is null.
+     */
     public void finishMatch(final String matchId) {
         ValidationUtils.requireNonNull(matchId);
 
         activeMatches.remove(matchId);
     }
 
+    /**
+     * Provides a summary of all active matches, sorted by total score in descending order.
+     * In case of a tie, matches are further sorted by their start time in reverse order.
+     *
+     * @return A list of {@code Match} objects representing the active matches.
+     */
     public List<Match> getMatchesSummary() {
         return activeMatches.values().stream()
                 .sorted(Comparator.comparingInt((Match m) -> m.getHomeScore() + m.getAwayScore())
--- src/main/java/org/sportradar/validator/MatchValidator.java
+++ src/main/java/org/sportradar/validator/MatchValidator.java
@@ -3,8 +3,26 @@ package org.sportradar.validator;
 import org.sportradar.exception.DuplicateCountryException;
 import org.sportradar.exception.InvalidCountryNameException;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
 
+/**
+ * The {@code MatchValidator} class is designed to validate country names in the context of sports matches.
+ * It employs a singleton pattern to ensure a single instance of the validator, preventing repeated initialization
+ * of the country list and avoiding static initialization blocks.
+ *
+ * <p>This class maintains a set of valid country names derived from ISO country codes, which it uses to
+ * validate the names of countries involved in matches. It offers methods for checking if country names are valid
+ * and if they are duplicates, throwing specific exceptions in each case.</p>
+ *
+ * <p>Note: In applications utilizing the Spring Framework, this class's functionality can be significantly
+ * simplified by using Spring beans. The singleton pattern here is manually implemented to avoid
+ * repeated initialization in non-Spring environments and to illustrate the pattern's usage.</p>
+ *
+ * @author Alexey Podkovko
+ */
 public class MatchValidator {
 
     private static class LazyHolder {
@@ -25,10 +43,23 @@ public class MatchValidator {
     private MatchValidator() {
     }
 
+    /**
+     * Returns the single instance of the {@code MatchValidator}, creating it if it does not exist.
+     *
+     * @return The single instance of {@code MatchValidator}.
+     */
     public static MatchValidator getInstance() {
         return LazyHolder.INSTANCE;
     }
 
+    /**
+     * Validates the provided country names.
+     *
+     * @param firstCountry The name of the first country to validate.
+     * @param secondCountry The name of the second country to validate.
+     * @throws InvalidCountryNameException If a country name is not in the set of valid countries.
+     * @throws DuplicateCountryException If the two country names are identical.
+     */
     public void validateCountries(final String firstCountry, final String secondCountry) throws InvalidCountryNameException, DuplicateCountryException {
         validateCountriesByName(firstCountry, secondCountry);
 
@@ -37,6 +68,12 @@ public class MatchValidator {
         }
     }
 
+    /**
+     * Validates each provided country name against the set of valid countries.
+     *
+     * @param countryNames The country names to validate.
+     * @throws InvalidCountryNameException If any country name is not in the set of valid countries.
+     */
     private void validateCountriesByName(final String... countryNames) throws InvalidCountryNameException {
         for (String countryName : countryNames) {
             if (!LazyHolder.COUNTRIES.contains(countryName)) {
